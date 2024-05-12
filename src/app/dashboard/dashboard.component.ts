d860dd9f26807bec233bc72be7182a48c043c900
Arpita.s <arpita.sahoo637174@gail.com>
2024-05-12 23:16:26 +0530

    style added and changed.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,57 @@
-import { Component, Inject, inject } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Inject, Pipe, PipeTransform, ViewChild, inject } from '@angular/core';
 import { GeminiService } from '../gemini.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewChecked{
 
   prompt:string = '';
   chatHistory:any[] = [];
   loading:boolean = false;
+  chatContainerHeight: number = 0;
+
+  @ViewChild('chatContainer') private chatContainer!: ElementRef;
+  @ViewChild('inputField') private inputField!: ElementRef;
 
   geminiSvc:GeminiService = inject(GeminiService);
-  constructor() { 
+  constructor(private sanitizer: DomSanitizer) { 
     this.geminiSvc.getMessageHistory().subscribe((data)=>{
       if (data) {
         this.chatHistory.push(data);
       }
     })
+  }
+
+
+  // After view checked lifecycle hook
+  ngAfterViewChecked() {
+    this.updateChatContainerHeight();
+    this.scrollToBottomIfNecessary();
+  }
+
+  updateChatContainerHeight() {
+    const inputHeight = this.inputField.nativeElement.offsetHeight;
+    this.chatContainerHeight = window.innerHeight - inputHeight;
+  }
+
+  scrollToBottomIfNecessary() {
+    const chatContainer = this.chatContainer.nativeElement;
+    const shouldScroll =
+      chatContainer.scrollHeight - chatContainer.scrollTop <= chatContainer.clientHeight;
+    if (shouldScroll) {
+      this.scrollToBottom();
+    }
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 
   async GetResponse(){
@@ -31,10 +64,6 @@ export class DashboardComponent {
     }
   }
 
-  FormatResponse(response: string): string {
-    const result = response.replaceAll('**',' '); 
-    return result; 
-  }
 
 
 }
